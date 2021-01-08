c4b255a48969b525283b648fca16774726a53437
Alexey <hughwols@gmail.com>
2021-01-08 20:30:56 +0200

    code review

---
+++ components/sections/features/Features.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="section features ">
+    <div class="container">
+      <div class="features__wrap--center">
+        <h2 class="section__title">
+          Наши режимы
+        </h2>
+        <p class="section__desc">
+          У нас не много режимов, но они смогут тебя зацепить и застаивть остаться с нами!
+        </p>
+        <div class="features__list features-list">
+          <ListItem
+            v-for="(item, index) in features"
+            :key="index"
+            :img="item.img"
+            :title="item.title"
+            :desc="item.desc"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ListItem from '~/components/sections/features/FeaturesListItem'
+
+export default {
+  name: 'Features',
+  components: { ListItem },
+  data () {
+    return {
+      features: [
+        {
+          img: require('~/assets/img/games/1.jpg'),
+          title: 'Creative',
+          desc: 'Строй ломай и побеждай'
+        }, {
+          img: require('~/assets/img/games/1.jpg'),
+          title: 'CastleWars',
+          desc: 'Красные против синих!'
+        }, {
+          img: require('~/assets/img/games/1.jpg'),
+          title: 'ThePit',
+          desc: 'Сражайся и будь в топе!'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,50 @@
 <template>
-  <div class=" page">
+  <div class="">
     <Hero />
-    <section class="section features">
-      <div class="container">
-        <div class="features__wrap--center">
-          <h2 class="section__title">
-            HighTwink
-          </h2>
-          <div class="features__list features-list">
-            <div class="features-list__item features-list__item--center">
-              <h3>
-                Версия
-              </h3>
-              <h4>
-                1.12.х - 1.16.х
-              </h4>
-            </div>
-            <div class="features-list__item features-list__item--center">
-              <h3>
-                IP
-              </h3>
-              <h4>
-                mc.hightwink.net
-              </h4>
-            </div>
-            <div class="features-list__item features-list__item--center">
-              <h3>
-                Онлайн
-              </h3>
-              <h4>
-                404 игрока
-              </h4>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="section features ">
-      <div class="container">
-        <div class="features__wrap--center">
-          <h2 class="section__title">
-            Наши режимы
-          </h2>
-          <p class="section__desc">
-            У нас не много режимов, но они смогут тебя зацепить и застаивть остаться с нами!
-          </p>
-          <div class="features__list features-list">
-            <ListItem
-              :img="require('~/assets/img/games/1.jpg')"
-              :title="'Creative'"
-              :desc="'Строй ломай и побеждай'"
-            />
-            <ListItem
-              :img="require('~/assets/img/games/1.jpg')"
-              :title="'CastleWars'"
-              :desc="'Красные против синих!'"
-            />
-            <ListItem
-              :img="require('~/assets/img/games/1.jpg')"
-              :title="'ThePit'"
-              :desc="'Сражайся и будь в топе!'"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+    <!--    <section class="section features">-->
+    <!--      <div class="container">-->
+    <!--        <div class="features__wrap&#45;&#45;center">-->
+    <!--          <h2 class="section__title">-->
+    <!--            HighTwink-->
+    <!--          </h2>-->
+    <!--          <div class="features__list features-list">-->
+    <!--            <div class="features-list__item features-list__item&#45;&#45;center">-->
+    <!--              <h3>-->
+    <!--                Версия-->
+    <!--              </h3>-->
+    <!--              <h4>-->
+    <!--                1.12.х - 1.16.х-->
+    <!--              </h4>-->
+    <!--            </div>-->
+    <!--            <div class="features-list__item features-list__item&#45;&#45;center">-->
+    <!--              <h3>-->
+    <!--                IP-->
+    <!--              </h3>-->
+    <!--              <h4>-->
+    <!--                mc.hightwink.net-->
+    <!--              </h4>-->
+    <!--            </div>-->
+    <!--            <div class="features-list__item features-list__item&#45;&#45;center">-->
+    <!--              <h3>-->
+    <!--                Онлайн-->
+    <!--              </h3>-->
+    <!--              <h4>-->
+    <!--                404 игрока-->
+    <!--              </h4>-->
+    <!--            </div>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--    </section>-->
+    <Features />
   </div>
 </template>
 
 <script>
 import Hero from '~/components/sections/Hero'
-import ListItem from '~/components/features/FeaturesListItem'
+import Features from '~/components/sections/features/Features'
+
 export default {
-  components: { Hero, ListItem }
+  components: { Hero, Features }
 }
 </script>
